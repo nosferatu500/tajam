08f4a98003b2149245bcb0bcfaf3d93871d67622
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-04-12 16:29:07 +0700

    Pre-Complete Testimonials

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,9 +1,50 @@
 <template>
-    <section>
-        Testimonals Section
+    <section class="s_testimonials">
+        <div class="container">
+            <div class="col-md-10 col-md-offset-1">
+                <h1>“</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus id delectus praesentium quia facilis nihil, dolore at, provident obcaecati molestias labore error sint voluptatem reprehenderit, aliquam repellat eaque officia molestiae.</p>
+                <h2>Dik Aladin</h2>
+                <h3>CEO TENGKUREP</h3>
+                <!--Add Owl-->
+            </div>
+        </div>
     </section>
 </template>
 
 <style lang="sass" scoped>
 
+.s_testimonials
+    background-image: url(../../src/assets/img/testimonials/bg.png)
+    background-size: cover
+    width: 100%
+    text-align: center
+    h1
+        font-family: Playfair_Display_Regular
+        padding: 0
+        margin: 0
+        font-size: 100px
+        padding-top: 65px
+        color: #fff
+    p
+        font-family: Playfair_Display_Regular
+        padding: 0
+        margin: 0
+        font-size: 16px
+        padding-bottom: 45px
+        color: #fff
+    h2
+        padding-top: 0
+        margin-top: 0
+        font-family: Roboto_Black
+        color: #fff
+        text-transform: uppercase
+        font-size: 14px
+    h3
+        font-family: Roboto_Light
+        color: #fff
+        text-transform: uppercase
+        font-size: 13px
+        padding-bottom: 65px
+        
 </style>
