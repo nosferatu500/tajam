fb5b9eb39be3f1d072896f54be81f24954b39183
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-04-12 18:53:22 +0700

    Pre-Complete Footer

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,24 +5,24 @@
                 <div class="col-md-6">
                     <h1>give us a good news</h1>
                     <form action="http://formspree.io/nosferatu2995@mail.ru" class="main_form" novalidate target="_blank" method="post">
-							<label class="form-group">
-								<input type="text" name="name" placeholder="Name" data-validation-required-message="Enter Name" required/>
-								<span class="help-block text-danger"></span>
-							</label>
-							<label class="form-group">
-								<input type="email" name="email" placeholder="Email" data-validation-required-message="Enter E-mail" required />
-								<span class="help-block text-danger"></span>
-							</label>
-                            <label class="form-group">
-								<input type="email" name="subject" placeholder="Subject" data-validation-required-message="Enter Subject" required />
-								<span class="help-block text-danger"></span>
-							</label>
-							<label class="form-group">
-								<textarea name="message" placeholder="Your Message" data-validation-required-message="Enter Message" required ></textarea>
-								<span class="help-block text-danger"></span>
-							</label>
-							<button>Submit</button>
-						</form>
+                        <label class="form-group">
+                            <input type="text" name="name" placeholder="Name" data-validation-required-message="Enter Name" required/>
+                            <span class="help-block text-danger"></span>
+                        </label>
+                        <label class="form-group">
+                            <input type="email" name="email" placeholder="Email" data-validation-required-message="Enter E-mail" required />
+                            <span class="help-block text-danger"></span>
+                        </label>
+                        <label class="form-group">
+                            <input type="email" name="subject" placeholder="Subject" data-validation-required-message="Enter Subject" required />
+                            <span class="help-block text-danger"></span>
+                        </label>
+                        <label class="form-group">
+                            <textarea name="message" placeholder="Your Message" data-validation-required-message="Enter Message" required ></textarea>
+                            <span class="help-block text-danger"></span>
+                        </label>
+                        <button>Submit</button>
+                    </form>
                 </div>
                 <div class="col-md-5">
                     <h1>our happy client</h1>
@@ -34,7 +34,7 @@
                             <img src="../../src/assets/img/contact/Converse.png" alt="">
                             <img src="../../src/assets/img/contact/Levis.png" alt="">
                         </div>
-                        <div class="c_right col-md-2">
+                        <div class="col-md-2 col-md-offset-4">
                             <img src="../../src/assets/img/contact/Mango.png" alt="">
                             <img src="../../src/assets/img/contact/Gucci.png" alt="">
                             <img src="../../src/assets/img/contact/NIKE.png" alt="">
@@ -51,6 +51,7 @@
 <style lang="sass" scoped>
 
 .s_contact
+    padding-bottom: 70px
     h1
         padding: 0
         margin: 0
@@ -94,6 +95,4 @@
                 padding-bottom: 50px
             .c_left
                 float: left
-            .c_right
-                float: right
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,102 @@
 <template>
-    <section>
-        Footer Section
-    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="logo">
+                        <img src="../../src/assets/img/header/logo.png" alt="">
+                        <h1>tajam</h1>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque numquam voluptates laudantium necessitatibus quibusdam fugit quo deleniti atque animi, amet voluptate fuga optio quisquam illum ab ipsam delectus iure quam?</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>our studio</h2>
+                    <p>Ruko cucruk, Jl. Radio luar dalem jos No.12 - 13, Kalideres - J</p>
+                    <p class="phone">(+62) 21-2224 3333</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>stay in touch</h2>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <label class="form-group">
+                                <input name="message" placeholder="Subscribe our newsletter" data-validation-required-message="Enter Message" required ></textarea>
+                                <span class="help-block text-danger"></span>
+                            </label>
+                        </div>
+                        <div class="col-md-4">
+                            <button>Subscribe</button>
+                        </div>
+                    </div>
+                    <h3>Develop by <a href="https://vk.com/nosferatu500">Vitaly Shvetsov</a></h3>
+                    <h3>Design by Aji</h3>
+                </div>
+            </div>
+            
+        </div>
+    </footer>
 </template>
 
 <style lang="sass" scoped>
 
+footer
+    background-image: url(../../src/assets/img/footer/bg.png)
+    background-size: cover
+    width: 100%
+    padding-top: 70px
+    padding-bottom: 50px
+    
+    .logo
+        img
+            padding-top: 15px
+            width: 45px
+            float: left
+        h1
+            font-family: Roboto_Thin
+            font-size: 30px
+            color: #3b7fa4
+            padding: 0
+            margin: 0
+            padding-top: 20px
+            padding-left: 10px
+            float: left
+    p
+        padding-top: 20px    
+        float: left
+        color: #fff
+    h2
+        padding-top: 15px
+        color: #fff
+        text-transform: uppercase
+        font-family: Roboto_Black
+        font-size: 15px
+    .phone
+        padding: 0
+        margin: 0
+    .form-group
+        display: block
+        margin-bottom: 20px
+        input
+            font-family: Roboto_Light
+            font-size: 14px
+            display: block
+            width: 100%
+            border: 1px solid #ccc
+            line-height: 43px
+            text-indent: 15px
+            background-color: transparent
+            color: #fff
+    button
+        font-family: Hammersmith_One  
+        border: 2px solid #00e0d0
+        background-color: transparent
+        text-transform: uppercase
+        color: #00e0d0
+        font-size: 16px
+        padding: 10px 20px
+        float: right
+
+    h3, a
+        padding: 0
+        margin: 0
+        color: #fff
 </style>
