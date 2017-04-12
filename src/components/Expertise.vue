395165c0fbbe8c9158a2d6b33be94081043b2d99
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-04-12 11:43:41 +0700

    Pre-Complete Expertise Section

--- src/components/Expertise.vue
+++ src/components/Expertise.vue
@@ -1,9 +1,86 @@
 <template>
     <section>
-        Expertise Section
+        <div class="container">
+            <div class="s_top">
+                <h1>Expertise</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="s_middle">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="../../src/assets/img/expertise/desktop.png" alt="">
+                        <h1>Web Design & Development</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil ea soluta sapiente nemo nostrum quae nesciunt culpa.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../../src/assets/img/expertise/brush.png" alt="">
+                        <h1>Branding Identity</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil ea soluta sapiente nemo nostrum quae nesciunt culpa.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../../src/assets/img/expertise/mobile.png" alt="">
+                        <h1>Mobile App</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil ea soluta sapiente nemo nostrum quae nesciunt culpa.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="../../src/assets/img/expertise/fragment.png" alt="">
+                        <h1>Search Engine Optimization</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil ea soluta sapiente nemo nostrum quae nesciunt culpa.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../../src/assets/img/expertise/gameboy.png" alt="">
+                        <h1>Game Development</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil ea soluta sapiente nemo nostrum quae nesciunt culpa.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../../src/assets/img/expertise/love.png" alt="">
+                        <h1>Made with Love</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil ea soluta sapiente nemo nostrum quae nesciunt culpa.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
 <style lang="sass" scoped>
+
+.s_top
+    text-align: center
+    h1
+        padding: 0
+        margin: 0
+        font-size: 22px
+        text-transform: uppercase
+        padding-top: 70px
+        padding-bottom: 20px
+    p
+        padding: 0
+        margin: 0
+        font-size: 14px
+
+.s_middle
+    text-align: center
+    padding-bottom: 100px
+    img
+        padding: 0
+        margin: 0
+        padding-top: 90px
+        padding-bottom: 30px
+
+    h1
+        padding: 0
+        margin: 0
+        text-transform: uppercase
+        font-size: 14px
+        padding-bottom: 20px
+    p
+        padding: 0
+        margin: 0
+        font-size: 14px
+        margin-left: 30px
+        margin-right: 30px
 
 </style>
