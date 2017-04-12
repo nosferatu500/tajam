fd46502f9430ea6e12335eedd3cc1b392b9dd0a7
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-04-12 12:17:17 +0700

    Pre-Complete Team

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,9 +1,95 @@
 <template>
-    <section>
-        Team Section
+    <section class="s_team">
+        <div class="container">
+            <div class="s_top">
+                <h1>meet our amazing team</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="s_middle">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="../../src/assets/img/team/employee.png" alt="">
+                        <h1>Dik aladin</h1>
+                        <p>CEO & Founder</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="../../src/assets/img/team/employee.png" alt="">
+                        <h1>Dik aladin</h1>
+                        <p>Engineering</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="../../src/assets/img/team/employee.png" alt="">
+                        <h1>Dik aladin</h1>
+                        <p>Designer</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="../../src/assets/img/team/employee.png" alt="">
+                        <h1>Dik aladin</h1>
+                        <p>Marketing</p>
+                    </div>
+                </div>
+            </div>
+            <div class="s_bottom">
+                <p>Become part of our dream team let`s join us!</p>
+                <button>We are hereing</button>
+            </div>
+        </div>
     </section>
 </template>
 
 <style lang="sass" scoped>
+
+.s_team
+    background-image: url(../../src/assets/img/team/bg.png)
+    background-size: cover
+    width: 100%
+    .s_top
+        padding-top: 70px
+        padding-bottom: 70px
+        text-align: center
+        h1
+            padding: 0
+            margin: 0
+            text-transform: uppercase
+            color: #fff
+            font-size: 22px
+        p
+            padding: 0
+            margin: 0
+            color: #fff
+            font-size: 14px
+    .s_middle
+        text-align: center
+        padding-bottom: 100px
+        h1
+            padding: 0
+            margin: 0
+            color: #fff
+            text-transform: uppercase
+            font-size: 14px
+            padding-top: 20px
+            padding-bottom: 10px
+        p
+            padding: 0
+            margin: 0
+            font-size: 13px
+            color: #fff
+            text-transform: uppercase
+    .s_bottom
+        text-align: center
+        padding-bottom: 65px
+        p
+            padding: 0
+            margin: 0
+            font-size: 14px
+            color: #fff
+            padding-bottom: 30px
+        button
+            border: 2px solid #3b7fa4
+            background-color: transparent
+            text-transform: uppercase
+            color: #3b7fa4
+            font-size: 16px
+            padding: 10px 15px
 
 </style>
